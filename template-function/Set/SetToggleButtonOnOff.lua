857e7841b081eda7cb2a0387d3effd9d181cbc33
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-07-16 03:39:23 +0800

    Create SetToggleButtonOnOff.lua

---
+++ template-function/Set/SetToggleButtonOnOff.lua
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+local function GetSetToggleButtonOnOff()
+    local value,ScriptWay,sec,cmd,mod,res,val = reaper.get_action_context()
+    local state = reaper.GetToggleCommandStateEx(sec, cmd) 
+    reaper.SetToggleCommandState(sec, cmd, math.abs(state - 1))
+    reaper.RefreshToolbar2(sec, cmd)
+    return state  
+end
+
+
+
+
+
+
+function SetToggleButtonOnOff(numb) -- 1 = On ; 0 = Off  
+    local value,ScriptWay,sec,cmd,mod,res,val = reaper.get_action_context()
+    reaper.SetToggleCommandState( sec, cmd, numb ) 
+    reaper.RefreshToolbar2( sec, cmd )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
