09fe4a1d208de1da0d3770687c7b4a6c86fa5c25
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-07-16 23:55:41 +0800

    Update Mouse_Is_Inside.lua

--- template-function/Gui/Mouse_Is_Inside.lua
+++ template-function/Gui/Mouse_Is_Inside.lua
@@ -1,27 +1,12 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
  -- координаты мыши для кнопки
  
       x - от левого края до кнопки
       y - от верха до кнопки
       w - от левого края кнопки до правого края кнопки
       h - от верхнего края кнопки до нижнего края кнопки
-
-
-
-
-
 
 
     local function Mouse_Is_Inside(x, y, w, h) --мышь находится внутри
@@ -31,6 +16,35 @@
         mouse_y >= y and mouse_y < (y + h)
         return inside
     end
+
+
+-----------
+
+
+  -- что бы действие срабатывало при отжатии кнопки
+
+        if gfx.mouse_cap&1 == 1  then  -- если левая кнопка нажата
+            if Mouse_Is_Inside(18,12,89,15) and not mouse_btn_down then
+               
+                --function()--когда нажата л.к.мыши --зажечь кнопку  
+                mouse_btn_down = true
+            end
+        else
+            if mouse_btn_down == true and Mouse_Is_Inside(18,12,89,15) then
+            msg('test', false )
+                --function() -- когда отжата л.к.мыши --выполнить действие
+                mouse_btn_down = false
+            else
+                mouse_btn_down = false
+            end
+        end
+
+
+
+
+
+
+
 
 
 
