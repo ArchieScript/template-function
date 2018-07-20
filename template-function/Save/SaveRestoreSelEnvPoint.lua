8932730d69e896917e95565e43649a8debde777b
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-07-21 04:02:10 +0800

    Update SaveRestoreSelEnvPoint.lua

--- template-function/Save/SaveRestoreSelEnvPoint.lua
+++ template-function/Save/SaveRestoreSelEnvPoint.lua
@@ -40,6 +40,34 @@
    
    
    
+
+
+---------------
+
+
+-   local selT 
+-   local function SaveRestoreSelEnvPoint(Env, SaveRest) 
+-       local t = nil
+-       if SaveRest == 1 then selT = {} end
+-       local CountAutoItem = reaper.CountAutomationItems(Env)
+-       for i = -1,CountAutoItem -1  do 
+-           local CountPoint =  reaper.CountEnvelopePointsEx(Env,i)
+-           for i1 = 1,CountPoint do 
+-               if SaveRest == 1 then
+-                   if not tonumber(t)then t = 0 end t = t + 1
+-                   local _,_,value,_,_,sel = reaper.GetEnvelopePointEx(Env,i,i1-1)
+-                   selT[t-1] = sel                 
+-               else  
+-                   if not tonumber(t)then t = (-1) end t = t + 1
+-                   for iT = 0, #selT  do
+-                       if iT == t then
+-                           reaper.SetEnvelopePointEx(Env,i,i1-1,nil,nil,nil,nil,selT[iT],nil)
+-                       end
+-                   end  
+-               end      
+-          end    
+-       end 
+-   end 
    
    
    
