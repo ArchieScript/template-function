4d2ad2940b67aaabeae1d1faff2820b2278157d3
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-07-28 02:32:58 +0800

    Update SaveRestoreSelEnvPoint.lua

--- template-function/Save/SaveRestoreSelEnvPoint.lua
+++ template-function/Save/SaveRestoreSelEnvPoint.lua
@@ -9,8 +9,10 @@
                    SaveRest = 1 Сохронить Save
                    SaveRest = 2 Востановить Restore
 
+                     ------------------------------
 
 
+                          -- Сохранение Востановление точек по времени /  подходит для удаления
 
     local function SaveRestoreSelEnvPoint(Env, SaveRest) 
         local t
@@ -43,31 +45,32 @@
 
 
 ---------------
+          -- Сохранение Востановление точек по номеру /  не кректно раб. с авто элементами
 
 
--   local selT 
--   local function SaveRestoreSelEnvPoint(Env, SaveRest) 
--       local t = nil
--       if SaveRest == 1 then selT = {} end
--       local CountAutoItem = reaper.CountAutomationItems(Env)
--       for i = -1,CountAutoItem -1  do 
--           local CountPoint =  reaper.CountEnvelopePointsEx(Env,i)
--           for i1 = 1,CountPoint do 
--               if SaveRest == 1 then
--                   if not tonumber(t)then t = 0 end t = t + 1
--                   local _,_,value,_,_,sel = reaper.GetEnvelopePointEx(Env,i,i1-1)
--                   selT[t-1] = sel                 
--               else  
--                   if not tonumber(t)then t = (-1) end t = t + 1
--                   for iT = 0, #selT  do
--                       if iT == t then
--                           reaper.SetEnvelopePointEx(Env,i,i1-1,nil,nil,nil,nil,selT[iT],nil)
--                       end
--                   end  
--               end      
--          end    
--       end 
--   end 
+   local selT 
+   local function SaveRestoreSelEnvPoint(Env, SaveRest) 
+       local t = nil
+       if SaveRest == 1 then selT = {} end
+       local CountAutoItem = reaper.CountAutomationItems(Env)
+       for i = -1,CountAutoItem -1  do 
+           local CountPoint =  reaper.CountEnvelopePointsEx(Env,i)
+           for i1 = 1,CountPoint do 
+               if SaveRest == 1 then
+                   if not tonumber(t)then t = 0 end t = t + 1
+                   local _,_,value,_,_,sel = reaper.GetEnvelopePointEx(Env,i,i1-1)
+                   selT[t-1] = sel                 
+               else  
+                   if not tonumber(t)then t = (-1) end t = t + 1
+                   for iT = 0, #selT  do
+                       if iT == t then
+                           reaper.SetEnvelopePointEx(Env,i,i1-1,nil,nil,nil,nil,selT[iT],nil)
+                       end
+                   end  
+               end      
+          end    
+       end 
+   end 
    
    
    
