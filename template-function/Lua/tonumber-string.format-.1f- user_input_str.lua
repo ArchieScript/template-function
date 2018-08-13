f676b112688b8cab0873de510d8e0bd4bb822cbc
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-08-14 01:28:52 +0800

    Update tonumber-string.format-.1f- user_input_str.lua

--- template-function/Lua/tonumber-string.format-.1f- user_input_str.lua
+++ template-function/Lua/tonumber-string.format-.1f- user_input_str.lua
@@ -8,14 +8,19 @@
 
 user_Numb = 15.6598745613
 
-Numb = tonumber(string.format("%.1f", user_input_str))
+Numb = tonumber(string.format("%.1f", user_Numb))
 
 на выходе 15.7
 
+-------------------------
 
 
+Numb = tonumber(string.format("%.2f", user_input_str))
+
+на выходе 15.66
 
 
+                   и т.д.
 
 
 
