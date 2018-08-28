bc75a3ff25717af28cfa159dabe202b16458296e
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-08-29 02:33:45 +0800

    Create GetIDByScriptName.lua

---
+++ template-function/Get/GetIDByScriptName.lua
@@ -0,0 +1,34 @@
+
+
+
+
+
+       ----Автор: MPL
+
+
+                --получить ID по имени скрипта
+                
+                
+                
+                
+ 
+function GetIDByScriptName(scriptname)
+    local fp, cont = reaper.GetResourcePath()..'/reaper-kb.ini'
+    local f = io.open(fp, 'a+')
+    if not f then return else cont = f:read('a') f:close() end
+    scriptname = scriptname:gsub('Script:%s' ,''):gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", function(c) return "%" .. c end)
+    for line in cont:gmatch('[^\r\n]+') do if line:match(scriptname) then return line:match('RS[%a%d]+') end end
+end
+
+ID = GetIDByScriptName( NameScript )
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
