b78f38fc1516062892e1f32875a2aff972b1581d
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-09-26 02:17:55 +0800

    Update Mouse_Is_Inside.lua

--- template-function/Gui/Mouse_Is_Inside.lua
+++ template-function/Gui/Mouse_Is_Inside.lua
@@ -44,7 +44,37 @@
 
 
 
+----=================================================================
+      
 
+   -- ТРЕБУЕСЯ Mouse_Is_Inside(x, y, w, h)
+
+
+      -- нажата л.к.мыши в этих координатах вернет 0
+      -- отжата л.к.мыши в этих координатах вернет 1
+      -- за приделами этих координатах вернет -1
+ 
+ 
+ local function LeftMouseButton(x, y, w, h)
+     if gfx.mouse_cap&1 == 1  then  -- если левая кнопка нажата
+         if Mouse_Is_Inside(x, y, w, h) and not fake then
+            mouse_btn_down = 0;fake = true  
+         end
+     elseif fake == true and Mouse_Is_Inside(x, y, w, h)then
+             mouse_btn_down = 1;fake = false
+         else
+             mouse_btn_down = -1;fake = false
+     end
+     return mouse_btn_down
+ end 
+
+
+ LeftMouse = LeftMouseButton(x, y, w, h)
+ if LeftMouse == 1 then 
+     --когда нажата л.к.мыши в этих координатах --зажечь кнопку;(или какое то действие) 
+ elseif LeftMouse == 2  then
+     -- когда отжата л.к.мыши в этих координатах --выполнить действие
+ end
 
 
 
