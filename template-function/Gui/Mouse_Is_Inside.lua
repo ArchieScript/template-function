42e60e9ca0b682c215ec45140d43f6b0259e091b
Archie <36672393+ArchieScript@users.noreply.github.com>
2018-09-26 03:38:47 +0800

    Update Mouse_Is_Inside.lua

--- template-function/Gui/Mouse_Is_Inside.lua
+++ template-function/Gui/Mouse_Is_Inside.lua
@@ -16,36 +16,16 @@
         mouse_y >= y and mouse_y < (y + h)
         return inside
     end
-
-
 -----------
 
 
 
-  -- что бы действие срабатывало при отжатии кнопки
-
-
-        if gfx.mouse_cap&1 == 1  then  -- если левая кнопка нажата
-            if Mouse_Is_Inside(18,12,89,15) and not mouse_btn_down then
-               
-                --function()--когда нажата л.к.мыши --зажечь кнопку  
-                mouse_btn_down = true
-            end
-        else
-            if mouse_btn_down == true and Mouse_Is_Inside(18,12,89,15) then
-
-                --function() -- когда отжата л.к.мыши --выполнить действие
-                mouse_btn_down = false
-            else
-                mouse_btn_down = false
-            end
-        end
-
-
+  
 
 
 ----=================================================================
       
+   -- что бы действие срабатывало при отжатии кнопки
 
    -- ТРЕБУЕСЯ Mouse_Is_Inside(x, y, w, h)
 
@@ -54,25 +34,37 @@
       -- отжата л.к.мыши в этих координатах вернет 1
       -- за приделами этих координатах вернет -1
  
+
+
  
- local function LeftMouseButton(x, y, w, h)
-     if gfx.mouse_cap&1 == 1  then  -- если левая кнопка нажата
-         if Mouse_Is_Inside(x, y, w, h) and not fake then
-            mouse_btn_down = 0;fake = true  
-         end
-     elseif fake == true and Mouse_Is_Inside(x, y, w, h)then
-             mouse_btn_down = 1;fake = false
-         else
-             mouse_btn_down = -1;fake = false
-     end
-     return mouse_btn_down
- end 
+local function LeftMouseButton(x, y, w, h)
+    if Mouse_Is_Inside(x, y, w, h) then
+        if gfx.mouse_cap&1 == 0 then fake = 1 end
+        if gfx.mouse_cap&1 == 1 and fake == 1 then  
+            mouse_btn_down = 0;   
+        end
+        if mouse_btn_down == 1 then mouse_btn_down = -1 end
+        if gfx.mouse_cap&1 == 0 and fake == 1 and mouse_btn_down == 0 then
+            mouse_btn_down = 1
+        end
+    else
+        mouse_btn_down = -1
+        if gfx.mouse_cap&1 == 1 and fake == 1 then 
+            mouse_btn_down = 0;  --fake = true  
+        end
+        if gfx.mouse_cap&1 == 0 then fake = nil end
+    end   
+    return mouse_btn_down
+end 
+
+
+
 
 
  LeftMouse = LeftMouseButton(x, y, w, h)
- if LeftMouse == 1 then 
+ if LeftMouse == 0 then 
      --когда нажата л.к.мыши в этих координатах --зажечь кнопку;(или какое то действие) 
- elseif LeftMouse == 2  then
+ elseif LeftMouse == 1 then
      -- когда отжата л.к.мыши в этих координатах --выполнить действие
  end
 
