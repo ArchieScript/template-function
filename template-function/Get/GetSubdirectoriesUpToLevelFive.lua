0242dc83688b05f1839a88f63e1bf24308f63e9d
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2018-11-14 23:47:12 +0800

    Update GetSubdirectoriesUpToLevelFive.lua

--- template-function/Get/GetSubdirectoriesUpToLevelFive.lua
+++ template-function/Get/GetSubdirectoriesUpToLevelFive.lua
@@ -29,6 +29,42 @@ local function GetSubdirectoriesUpToLevelFive(Path);
 end;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local function GetSubdirectoriesUpToLevelFive(Path);
+  local T = {} Enu = reaper.EnumerateSubdirectories;s="\\";h=math.huge;
+  for i=0,h do;f1 = Enu(Path,i);if f1 then;T[#T+1]=Path..s..f1;
+    for i2=0,h do;f2=Enu(Path..s..f1,i2)if f2 then T[#T+1]=Path..s..f1..s..f2;
+      for i3=0,h do;f3=Enu(Path..s..f1..s..f2,i3)if f3 then T[#T+1]=Path..s..f1..s..f2..s..f3;
+        for i4=0,h do;f4 = Enu(Path..s..f1..s..f2..s..f3,i4)if f4 then T[#T+1]=Path..s..f1..s..f2..s..f3..s..f4;
+          for i5=0,h do;f5=Enu(Path..s..f1..s..f2..s..f3..s..f4,i5)if f5 then T[#T+1]=Path..s..f1..s..f2..s..f3..s..f4..s..f5;
+          ----------
+          end;if not f5 then break end;end;
+        end;if not f4 then break end;end;
+      end;if not f3 then break end;end;
+    end;if not f2 then break end;end; 
+  end;if not f1 then return T end;end;
+end;
+
+
+
+
+
+
+
+
 Path = reaper.GetResourcePath() -- returns path where ini files are stored, other things are in subdirectories.
 Bla = GetSubdirectoriesUpToLevelFive(Path)  
 
