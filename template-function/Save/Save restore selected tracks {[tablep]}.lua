f169f9549f180fd0279c92bde23243566c46a534
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2018-12-05 20:51:17 +0800

    Update Save restore selected tracks {[tablep]}.lua

--- template-function/Save/Save restore selected tracks {[tablep]}.lua
+++ template-function/Save/Save restore selected tracks {[tablep]}.lua
@@ -54,3 +54,41 @@
 
 
 
+
+
+
+    -------SaveSelTracksGuidSlot---------------------------------------------------------
+    ------RestoreSelTracksGuidSlot-------------------------------------------------------
+    function Arc_Module.SaveSelTracksGuidSlot(Slot);
+        local t = {};
+        _G[Slot] = t;
+        for i = 1, reaper.CountSelectedTracks(0) do;
+            local sel_tracks = reaper.GetSelectedTrack(0,i-1);
+            t[i] = reaper.GetTrackGUID(sel_tracks);
+        end;
+    end;
+    ---
+    function Arc_Module.RestoreSelTracksGuidSlot(Slot,reset);
+       local tr = reaper.GetTrack(0,0);
+       reaper.SetOnlyTrackSelected(tr);
+       reaper.SetTrackSelected(tr, 0);
+       ---
+       local t = _G[Slot];
+       for i = 1, #t do;
+           local track = reaper.BR_GetMediaTrackByGUID(0,t[i]);
+           if track then;
+               reaper.SetTrackSelected(track,1);
+           end;
+       end;
+       if reset == 1 or reset == true then;
+           _G[Slot] = nil;
+       end;
+    end;
+    -- SaveSelTracksGuidSlot("Slot_1")
+    -- RestoreSelTracksGuidSlot("Slot_1",true)
+    -- SaveSelTracksGuidSlot("Slot_2")
+    -- RestoreSelTracksGuidSlot("Slot_2",false)
+    -- RestoreSelTracksGuidSlot("Slot_2",true)
+    --====End===============End===============End===============End===============End====
+
+
