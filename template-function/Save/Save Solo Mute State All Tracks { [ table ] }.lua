680b2163b594a232545d4c13e268593facc7f504
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2018-12-05 22:08:51 +0800

    Update Save Solo Mute State All Tracks { [ table ] }.lua

--- template-function/Save/Save Solo Mute State All Tracks { [ table ] }.lua
+++ template-function/Save/Save Solo Mute State All Tracks { [ table ] }.lua
@@ -34,3 +34,50 @@
 
 
 
+    -------SaveSoloMuteStateAllTracksSlot()----------------------------------------------
+    --------------------------------------RestoreSoloMuteStateAllTracksSlot()------------
+    function SaveSoloMuteStateAllTracksSlot(Slot);
+        local CountTracks = reaper.CountTracks(0);
+        if CountTracks == 0 then return false end;
+        local t = {};_G['SavSolMutTrSlot_'..Slot] = t;
+        for i = 1, CountTracks do;
+            local track = reaper.GetTrack(0, i - 1);
+            t[i] = reaper.GetTrackGUID(track)..'{'..
+            reaper.GetMediaTrackInfo_Value(track,'B_MUTE')..'}{'..
+            reaper.GetMediaTrackInfo_Value(track,'I_SOLO')..'}';
+        end;
+    end;
+    -----------------------------------------------------------------
+    function RestoreSoloMuteStateAllTracksSlot(Slot,clean);
+        local t = _G['SavSolMutTrSlot_'..Slot];
+        if t then;
+            for i = 1, #t do;
+                local guin,mute,solo = string.match (t[i],'({.+}){(.+)}{(.+)}');
+                local track = reaper.BR_GetMediaTrackByGUID(0,guin);
+                reaper.SetMediaTrackInfo_Value(track, 'B_MUTE', mute);
+                reaper.SetMediaTrackInfo_Value(track, 'I_SOLO', solo);
+            end;
+            if clean == 1 or clean == true then;
+                _G['SavSolMutTrSlot_'..Slot] = nil;
+                t = nil;
+            end;
+        end;    
+    end;
+    -- Save Restore Solo Mute State All Tracks, Slots
+    -- Сохранить Восстановить Соло Mute Состояние Всех Дорожек, Слоты
+    -- clean = true или 1 - зачистить сохраненную информацию за собой
+    --====End===============End===============End===============End===============End==== 
+
+
+
+
+
+    Arc_Module.SaveSoloMuteStateAllTracksSlot(1)
+    Arc_Module.RestoreSoloMuteStateAllTracksSlot(1,false)
+
+    reaper.Main_OnCommand(id,0)
+ 
+ 
+   Arc_Module.RestoreSoloMuteStateAllTracksSlot(1,true)
+
+
