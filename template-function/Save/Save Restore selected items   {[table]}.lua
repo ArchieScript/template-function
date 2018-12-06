d0081961ec1cbbd0a34c9b9856c8f04322300ec5
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2018-12-06 14:29:59 +0800

    Update Save Restore selected items   {[table]}.lua

--- template-function/Save/Save Restore selected items   {[table]}.lua
+++ template-function/Save/Save Restore selected items   {[table]}.lua
@@ -58,7 +58,38 @@
 
 
 
-
+    -----------Save_Selected_Items_Slot()------------------------------------------------
+    -------------------------------------Restore_Selected_Items_Slot()-------------------
+    function Arc_Module.Save_Selected_Items_GuidSlot(Slot);
+        local CountSelItem = reaper.CountSelectedMediaItems(0);
+        if CountSelItem == 0 then return false end;
+        local t = {};
+        _G["SaveSelItem_"..Slot] = t;
+        for i = 1, CountSelItem do;
+            local sel_item = reaper.GetSelectedMediaItem(0,i-1);
+            t[i] = reaper.BR_GetMediaItemGUID(sel_item);
+        end;
+        return true;
+    end;
+    ------------------------------------------------------
+    function Arc_Module.Restore_Selected_Items_GuidSlot(Slot,clean);
+        t = _G["SaveSelItem_"..Slot];
+        if t then;
+            reaper.SelectAllMediaItems(0,0);
+            for i = 1, #t do;
+                local item = reaper.BR_GetMediaItemByGUID(0,t[i]);
+                if item then;
+                    reaper.SetMediaItemSelected(item,1);
+                end;
+            end;
+            if clean == true or clean == 1 then;
+                _G["SaveSelItem_"..Slot] = nil;
+                t = nil;
+            end;
+            reaper.UpdateArrange();
+        end;
+    end;
+    --====End===============End===============End===============End===============End====
 
 
 
