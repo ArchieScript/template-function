e1b7112203842f4c15712c41fd09f921cf48ab83
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-03-26 02:40:06 +0800

    Update Create Folder.lua

--- template-function/Rest/Create Folder.lua
+++ template-function/Rest/Create Folder.lua
@@ -12,3 +12,19 @@
     newFile = io.open( Path.."/"..dirname.."\\File.txt", "w+" ) -- windows
     newFile:write( "fghgljklhjg" )
     newFile:close()
+
+
+
+
+
+
+
+------------------------
+  -- mac ???
+
+-- os.execute('mkdir -p ~/Documents/"Your folder"')
+
+--------------------------
+
+
+--https://qasseta.ru/q/116798/не-удается-создать-папку-в-lua-на-всех-платформах
