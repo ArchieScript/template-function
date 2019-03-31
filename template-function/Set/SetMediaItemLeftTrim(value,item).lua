d6355a3bf481e4cada6dce57fbd11cbecae1a511
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-03-31 18:16:39 +0800

    Update SetMediaItemLeftTrim(value,item).lua

--- template-function/Set/SetMediaItemLeftTrim(value,item).lua
+++ template-function/Set/SetMediaItemLeftTrim(value,item).lua
@@ -3,26 +3,7 @@
   
          -- Set Media Item Left Trim
 
-                                                      (баг)
-  
-  local function SetMediaItemLeftTrim(value,item)
-        local Pos = reaper.GetMediaItemInfo_Value( item, 'D_POSITION' )
-        local len = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH' )
-        local take = reaper.GetActiveTake( item )
-        local playrate = reaper.GetMediaItemTakeInfo_Value( take, 'D_PLAYRATE')
-        local offset = reaper.GetMediaItemTakeInfo_Value( take, 'D_STARTOFFS' )
-        if len > value then
-            reaper.SetMediaItemLength( item,  value , true )
-            reaper.SetMediaItemPosition( item,(len-value)+ Pos, true )
-            reaper.SetMediaItemTakeInfo_Value( take, 'D_STARTOFFS',offset + (len-value)*playrate )
-        else
-            if Pos < (value-len) then return end
-            reaper.SetMediaItemLength( item, value , true )
-            reaper.SetMediaItemPosition( item, Pos-(value-len), true )
-            reaper.SetMediaItemTakeInfo_Value( take, 'D_STARTOFFS',offset + (len-value)*playrate )
-        end
-    end
-    
+
     
     
     
