a58f5cb7518e4c71bc0e13b97793d69f2c9d0ca4
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-05-11 21:41:38 +0800

    Update GetScriptNameByID.lua

--- template-function/Get/GetScriptNameByID.lua
+++ template-function/Get/GetScriptNameByID.lua
@@ -22,14 +22,30 @@
 
 
 
-----------------------------------------
--- Готов:
 
+
+----------------------------------------
+-- ГОТОВ:
+
+    
     function GetScriptNameByID(id);
+        if type(id)~="string"then error("expects a 'string', got "..type(id),2) end;
         local file = io.open(reaper.GetResourcePath()..'/reaper-kb.ini','r');
-        if file then;local text = file:read('a');file:close();
-            return text:match(id:match('[^_](%S+)')..'%s-"Custom:%s-.(.-)"')or-1;
-    end;end;
+        if not file or (#id<1) then return -1 end;local text = file:read('a');file:close();
+        id = id:match('%_*(%S+)');return
+        text:match("%s"..id..'%s-"Custom:%s-.(.-)"')or
+        text:match('"'..id..'"%s-"Custom:%s-.(.-)"')or-1;
+    end;
+    
+
+
+
+
+
+
+
+
+
 
 
 
