66404d7d3717d7dc43434825c329929041e5c93c
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-07-10 19:17:14 +0800

    Update CopyMoveMediaItem.lua

--- template-function/Get/CopyMoveMediaItem.lua
+++ template-function/Get/CopyMoveMediaItem.lua
@@ -4,8 +4,13 @@
 
 
     -- Копировать или переместить элемент мультимедиа.
+    -- item = какой элемент переместить
+    -- track = на какой трек переместить, если трек не указан, то перемесщение произойдет на родительском треке элемента
+
     -- COPY_MOVE = 0 переместить, = 1 Копировать.
-    -- Вернет время и новый  элемент при перемещении или время и старый при копировании. 
+
+
+    -- Вернет позицию и новый  элемент при перемещении или позицию и старый при копировании. 
      
      
      
@@ -13,27 +18,30 @@
      
      
      
-    function CopyMoveMediaItem(item, time, COPY_MOVE);        
+    function CopyMoveMediaItem(item, track, time,  COPY_MOVE);        
         local var2;
-        local retval, str = reaper.GetItemStateChunk(item,"",false);
+        local pcal, retval, str = pcall(reaper.GetItemStateChunk,item,"",false);
+        if not pcal then;
+            error("Media Item expected",2);
+        end;
         for var in string.gmatch (str,".-\n") do;
            var = var:gsub("GUID%s-{.-}","GUID "..reaper.genGuid());
            var2 = (var2 or "")..var;
         end;
         ----
-        if COPY_MOVE == 1 then; --1copy
-            reaper.PreventUIRefresh(9876);
-            local It_Track = reaper.GetMediaItem_Track(item);
-            local newItem = reaper.CreateNewMIDIItemInProj(It_Track,time,time+0.1,false);
-            reaper.SetItemStateChunk(newItem,var2,false);
-            reaper.SetMediaItemInfo_Value(newItem,"D_POSITION",time);
-            reaper.PreventUIRefresh(-9876);
-            local newPos = reaper.GetMediaItemInfo_Value(newItem,"D_POSITION");
-            return newPos,newItem;
-        else;
-            reaper.SetMediaItemInfo_Value(item,"D_POSITION",time);
-            return time,item;
-        end;          
+        if not pcall(reaper.GetTrackColor,track) then;
+            track = reaper.GetMediaItem_Track(item);
+        end;
+        reaper.PreventUIRefresh(9876);
+        local newItem = reaper.CreateNewMIDIItemInProj(track,time,time+0.1,false);
+        reaper.SetItemStateChunk(newItem,var2,false);
+        reaper.SetMediaItemInfo_Value(newItem,"D_POSITION",time);
+        local newPos = reaper.GetMediaItemInfo_Value(newItem,"D_POSITION");
+        reaper.PreventUIRefresh(-9876);
+        if COPY_MOVE ~= 1 then;
+            reaper.DeleteTrackMediaItem(reaper.GetMediaItem_Track(item),item); 
+        end;
+        return newPos,newItem;          
     end;
     
     
