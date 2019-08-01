6ae982413f2030b7176654b92b659b799077dd58
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-08-01 23:35:34 +0800

    Create Create Folder.lua

---
+++ template-function/Lua/Create Folder.lua
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+    Path = "C:\\Users\\User\\Desktop\\Reaper"
+    dirname = "NewFolder"
+    
+    --NewFolder--
+    os.execute('mkdir "'..Path..'\\'.. dirname..'"') -- windows
+    
+    --NewFile--
+    newFile = io.open( Path.."/"..dirname.."\\File.txt", "w+" ) -- windows
+    newFile:write( "fghgljklhjg" )
+    newFile:close()
+
+
+
+
+
+
+
+------------------------
+  -- mac ???
+
+-- os.execute('mkdir -p ~/Documents/"Your folder"')
+
+--------------------------
+
+
+--https://qasseta.ru/q/116798/не-удается-создать-папку-в-lua-на-всех-платформах
+
+
+
+
+
+
+
+
+
+
