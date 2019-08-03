db20017f6b45511bc882ff5d9dbcfaaeafee1f38
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-08-03 23:48:50 +0800

    Update copyFile.lua

--- template-function/Lua/copyFile.lua
+++ template-function/Lua/copyFile.lua
@@ -41,7 +41,48 @@
 
 
 
+--======================================================
 
+
+
+
+
+
+
+    local function copyFile(file1,file2);
+        local in_file = io.open(file1,"rb");
+        if not in_file then return false end;
+        local in_str = in_file:read("*a");
+        in_file:close();
+        ----/Один и тот же файл/----
+        local check_file = io.open(file2,"rb");
+        if check_file then;
+            local check_file_str = check_file:read("*a");
+            check_file:close();
+            if in_str == check_file_str then;
+                return -1, file2;
+            end;
+        end;
+        ----------------------------
+        local x,i = file2;
+        while true do;
+            i = (i or 0)+1;
+            local check_file = io.open(file2,"r");
+            if not check_file then break end;
+            check_file:close();
+            file2 = x;
+            local pach, name = file2:match("(.+)[/\\](.+)");
+            local name_no_extension = name:match("(.+)%..-$") or name:match(".+$");
+            local extension = name:match(".+(%..-)$");
+            file2 = pach.."/"..name_no_extension.."_"..i..extension;
+        end;
+        ----
+        local out_file = io.open(file2,"wb");
+        if not out_file then return false end;
+        out_file:write(in_str);
+        out_file:close();
+        return true, file2;
+    end;
 
 
 
