fb1fccacf796e1b89512ce2efb931f8389c52eab
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-08-12 16:46:35 +0800

    Update Envelope_Point.lua

--- template-function/Rest/Envelope_Point.lua
+++ template-function/Rest/Envelope_Point.lua
@@ -1,8 +1,27 @@
+        -- ENVELOPE_POINT
+        
+        InsertEnvelopePointEx_Arc(...);     --40
+        InsertEnvelopePointTake_Arc(...);   --88
+        InsertEnvelopePointTrack_Arc(...);  --127
         
         
         
         
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         
    --==========================================================================================================================
