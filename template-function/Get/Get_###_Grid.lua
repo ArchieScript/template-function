12249a7fc5ae3d9aa3a13b0c2cb0abd10583c079
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-09-01 02:28:49 +0800

    Update Get_###_Grid.lua

--- template-function/Get/Get_###_Grid.lua
+++ template-function/Get/Get_###_Grid.lua
@@ -1,11 +1,9 @@
 
 
 
-
+----------------------------------------------------------
 -- Реагирует на привязку сетки (вкл/выкл)
 -- Ищет только видимую сетку
-
-
 
 
     local function GetPreviousGrid(time);
@@ -26,7 +24,27 @@
             time = time+0.001;
         end;
     end;
+-----------------------------------------------------
 
+
+
+-- Не реагирует на привязку сетки (вкл/выкл)
+-- Ищет только видимую сетку
+-- Arrange
+
+    local function GetPreviousGridArrange(time);
+        local ToggleSnap = reaper.GetToggleCommandStateEx(0,1157);
+        if ToggleSnap == 0 then;reaper.Main_OnCommand(1157,0);end;
+        for i = 1,math.huge do;
+            local val = reaper.SnapToGrid(0,time);
+            if val <= time then
+                if ToggleSnap == 0 then;reaper.Main_OnCommand(1157,0);end;return val;
+            end;
+            time = time-0.001;
+        end;
+    end;
+
+-----------------------------------------------------------
 
 
 
