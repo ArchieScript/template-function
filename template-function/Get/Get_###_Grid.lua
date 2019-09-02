cb5e995c235eb9669082c353c1742d9cb95e7b70
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-09-02 14:49:18 +0800

    Update Get_###_Grid.lua

--- template-function/Get/Get_###_Grid.lua
+++ template-function/Get/Get_###_Grid.lua
@@ -52,23 +52,23 @@
 -- Arrange
 
 
-    local function GetPrevNextGridArrange(time,nextPrev);--< 0 Prev / >=0 Next
+    local function GetPrevNextGridArrange(time,nextPrev);
         local ToggleSnap = reaper.GetToggleCommandStateEx(0,1157);
         if ToggleSnap == 0 then;reaper.Main_OnCommand(1157,0);end;
+        local ToggleEnab = reaper.GetToggleCommandStateEx(0,40145);
+        if ToggleEnab == 0 then;reaper.Main_OnCommand(40145,0);end;
+        local val;
         for i = 1,math.huge do;
-            local val = reaper.SnapToGrid(0,time);
+            val = reaper.SnapToGrid(0,time);
             if nextPrev < 0 then;
-                if val <= time then;-- <
-                    if ToggleSnap == 0 then;reaper.Main_OnCommand(1157,0);end;return val;
-                end;
-                time = time-0.001;
+                if val<=--[[<]]time then;goto ret;end;time=time-0.001;
             else;
-                if val > time then; -- >=
-                    if ToggleSnap == 0 then;reaper.Main_OnCommand(1157,0);end;return val;
-                end;
-                time = time+0.001;
-            end;   
-        end;
+                if val>--[[>=]]time then;goto ret;end;time=time+0.001;
+            end;
+        end;::ret::;
+        if ToggleSnap == 0 then;reaper.Main_OnCommand(1157 ,0);end;
+        if ToggleEnab == 0 then;reaper.Main_OnCommand(40145,0);end;
+        return val;
     end;
 
 
