9da1d31c27bd2578af15c3898758c4730275ec83
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-09-04 20:22:46 +0800

    Create Compress2.lua

---
+++ template-function/Function-bak/Compress2.lua
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+    local 
+    path,scr_file = reaper.GetResourcePath()..'/Scripts/Archie-ReaScripts/Functions/','Arc_Function_lua.lua';
+    local
+    scr_file_New = "!!!"..scr_file;
+    
+    
+    -----------------------------------------
+    local file = io.open(path..scr_file,'r');
+    if not file then error("  There is no file !!! / Нет файла !!!  "..path,1)end;
+    local text = file:read('a')..'\n';
+    file:close();
+    -------------
+    
+    
+    
+    --- / Комент.Одностр / -----------
+    for var in string.gmatch(text,".-\n")do;
+	   if var:match(".-(%-%-[^%[%]]+\n)")then
+		  var =  var:match("(.-)%-%-[^%[%]]+\n").."\n"
+	   end
+	   var2 = (var2 or "")..var;
+    end;     
+	   
+    
+    --- / Удалить '\n' / -------------
+    for var in string.gmatch(var2,".-\n")do;
+	   if var:match("^%s-\n")then;
+		  var = "";
+	   end;
+	   header = (header or "")..var;
+    end;
+	 
+    
+   
+   
+   --- / Записываем в файл / ------    
+   file = io.open(path..scr_file_New,"w");
+   file:write(header);
+   file:close();
+   -------------------------------- 
