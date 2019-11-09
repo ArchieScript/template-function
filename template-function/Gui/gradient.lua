5593775f2446d90f47619b51a80f6df3813b69c3
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-11-10 00:06:13 +0800

    Update gradient.lua

--- template-function/Gui/gradient.lua
+++ template-function/Gui/gradient.lua
@@ -23,20 +23,25 @@
     
     
     
-    ---------------------------------
+    --====================================================================
     local function gradient(buf,vertically,x,y,w,h,white,black,alfa,col,brightness,frame);
         if h <= 5 then h = 0 end;
         if w <= 5 then w = 0 end;
         buf = tonumber(buf)or 948;if buf<0 then buf=0 end;if buf>1023 then buf=1023 end;
         local b = brightness;
+        if b < -1 then b = -1 elseif b > 1 then b = 1 end;
         alfa = tonumber(alfa)or 1;
         if alfa < 0 then alfa = 0 elseif alfa > 1 then alfa = 1 end;
-        if type(col) ~= "table" then;
-            col = { {1,0,0},{1,0.25,0},{1,0.5,0},{1,0.75,0},{1,1,0},{0.75,1,0},{0.5,1,0},
-                    {0.25,1,0},{0,1,0},{0,1,0.25},{0,1,0.5},{0,1,0.75},{0,1,1},{0,0.75,1},
-                    {0,0.50,1},{0,0.25,1},{0.25,0,1},{0.5,0,1},{0.75,0,1},{1,0,1},{1,0,0.75},
-                    {1,0,0.5},{1,0,0.25},{1,0,0} };
+        if type(col) ~= "table" or type(col[1])~= "table" then;
+            col = {{1,0,0},{1,.1,0},{1,.2,0},{1,.3,0},{1,.4,0},{1,.5,0},{1,.6,0},{1,.7,0},{1,.8,0},{1,.9,0},
+                   {1,1,0},{.9,1,0},{.8,1,0},{.7,1,0},{.6,1,0},{.5,1,0},{.4,1,0},{.3,1,0},{.2,1,0},{.1,1,0},
+                   {0,1,0},{0,1,.1},{0,1,.2},{0,1,.3},{0,1,.4},{0,1,.5},{0,1,.6},{0,1,.7},{0,1,.8},{0,1,.9},
+                   {0,1,1},{0,.9,1},{0,.8,1},{0,.7,1},{0,.6,1},{0,.5,1},{0,.4,1},{0,.3,1},{0,.2,1},{0,.1,1},
+                   {0,0,1},{.1,0,1},{.2,0,1},{.3,0,1},{.4,0,1},{.5,0,1},{.6,0,1},{.7,0,1},{.8,0,1},{.9,0,1},
+                   {1,0,1},{1,0,.9},{1,0,.8},{1,0,.7},{1,0,.6},{1,0,.5},{1,0,.4},{1,0,.3},{1,0,.2},{1,0,.1},
+                   {1,0,0}};
         end;
+        if #col == 1 then col[2] = col[1] end;
         -----
         local gfx_r,gfx_g,gfx_b,gfx_a = gfx.r,gfx.g,gfx.b,gfx.a;
         gfx.dest = buf;
@@ -61,27 +66,27 @@
                              0);
             end;
             -----
+            ----- 
             if white == true then;
-                local rw = b;
-                if rw < 0 then rw = 0 elseif rw > 1 then rw = 1 end;
-                rw = ((rw-1)-(rw-1)*2)*100;
-                local rwa=((b-1)-(b-1)*2) if rwa > 1 then rwa = 1 elseif rwa < 0 then rwa = 0 end;
-                gfx.gradrect(0,0,(#col-1)*10,rw, 0,0,0,rwa, 0,0,0,0, 0,0,0,(0-rwa)/rw);
+                local rw = b+1;
+                if rw > 1 then rw = 1 elseif rw < 0 then rw = 0 end;
+                gfx.gradrect(0,0,(#col-1)*10,5, rw,rw,rw,1);
+                gfx.gradrect(0,5,(#col-1)*10,95, rw,rw,rw,1, 0,0,0,0, 0,0,0,(0-1)/(95));
             end;
             if black == true then;
                 local rb = b;
-                if rb > 0 then rb = 0 elseif rb < -1 then rb = -1 end;
-                rb = (rb-rb*2)*100;
-                local rba=b+1 if rba > 1 then rba = 1 elseif rba < 0 then rba = 0 end;
-                gfx.gradrect(0,100+rb,(#col-1)*10,100, 1,1,1,0, 0,0,0,0, 0,0,0,rba/100);
+                if rb > 1 then rb = 1 elseif rb < 0 then rb = 0 end;
+                gfx.gradrect(0,195,(#col-1)*10,5, rb,rb,rb,1);
+                gfx.gradrect(0,100,(#col-1)*10,95, rb,rb,rb,0, 0,0,0,0, 0,0,0,1/(95));
             end;
             ----
             local rwa = b;
+            if white == true then rwa = rwa*2-1 end;
             if rwa < 0 then rwa = 0 elseif rwa > 1 then rwa = 1 end;
-            gfx.gradrect(0,0,(#col-1)*10,200,1,1,1,rwa); 
+            gfx.gradrect(0,0,(#col-1)*10,200,1,1,1,rwa);
             ----
-            local rba = b;
-            rba = b-b*2;
+            local rba = b-b*2;
+            if black == true then rba = (rba*2-1)end;
             if rba < 0 then rba = 0 elseif rba > 1 then rba = 1 end;
             gfx.gradrect(0,0,(#col-1)*10,200,0,0,0,rba);
             -----
@@ -96,29 +101,28 @@
             end;
             ----
             if white == true then;
-                local rw = b;
-                if rw > 0 then rw = 0 elseif rw < -1 then rw = -1 end;
-                rw = (rw-rw*2)*100;
-                local rwa=b+1 if rwa > 1 then rwa = 1 elseif rwa < 0 then rwa = 0 end;
-                gfx.gradrect(100+rw,0,100,(#col-1)*10, 1,1,1,0, 0,0,0,rwa/100, 0,0,0,0);
+                local rw = b+1;
+                if rw < 0 then rw = 0 elseif rw > 1 then rw = 1 end;
+                gfx.gradrect(195,0,5,(#col-1)*10, rw,rw,rw,1);
+                gfx.gradrect(100,0,95,(#col-1)*10, rw,rw,rw,0, 0,0,0,1/95, 0,0,0,0);
             end;
             if black == true then;
                 local rb = b;
-                if rb < 0 then rb = 0 elseif rb > 1 then rb = 1 end;
-                rb = ((rb-1)-(rb-1)*2)*100;
-                local rba=((b-1)-(b-1)*2) if rba > 1 then rba = 1 elseif rba < 0 then rba = 0 end;
-                gfx.gradrect(0,0,rb,(#col-1)*10, 0,0,0,rba, 0,0,0,(0-rba)/rb, 0,0,0,0);
+                if rb > 1 then rb = 1 elseif rb < 0 then rb = 0 end;
+                gfx.gradrect(0,0,5,(#col-1)*10, rb,rb,rb,1);
+                gfx.gradrect(0,0,95,(#col-1)*10, rb,rb,rb,1, 0,0,0,(0-1)/95, 0,0,0,0);
             end;
             ----
-            local rba = b;
-            rba = b-b*2;
-            if rba < 0 then rba = 0 elseif rba > 1 then rba = 1 end;
-            gfx.gradrect(0,0,200,(#col-1)*10,0,0,0,rba);
-            ----
             local rwa = b;
+            if white == true then rwa = rwa*2-1 end;
             if rwa < 0 then rwa = 0 elseif rwa > 1 then rwa = 1 end;
             gfx.gradrect(0,0,200,(#col-1)*10,1,1,1,rwa);
             ----
+            local rba = b-b*2;
+            if black == true then rba = (rba*2-1)end;
+            if rba < 0 then rba = 0 elseif rba > 1 then rba = 1 end;
+            gfx.gradrect(0,0,200,(#col-1)*10,0,0,0,rba);
+            ----
         end;
         gfx.dest = -1;
         gfx.a = alfa;
@@ -127,139 +131,10 @@
         else;
             x_s,y_s,w_s,h_s = 0,1,10*(#col-1),198;
         end;
-        gfx.blit(buf,1,0, x_s,y_s,w_s,h_s, x,y,w,h, 0,0);
-        ----
-        if type(frame)~="table" then frame = {} end;
-        gfx.r,gfx.g,gfx.b,gfx.a = frame[2]or 0,frame[3]or 0,frame[4]or 0,frame[5]or 0;
-        gfx.a = math.min(math.max(0,gfx.a),1);
-        for i = 1,(frame[1]or 1) do;
-            gfx.roundrect( x+(i-1) , y+(i-1), w-((i-1)*2) , h-((i-1)*2), 1);
-        end;
-        ---
-        gfx.r,gfx.g,gfx.b,gfx.a = gfx_r,gfx_g,gfx_b,gfx_a;
-    end;
-    ---------------------------------
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
---============================================
---============================================
-
-
-
-
-
-
-    -- white ,black по отдельности не отключается, либо false,false, либо true,true
-    -- при  white,black=true,true поведение яркости другое 
-    -- https://avatars.mds.yandex.net/get-pdb/2129179/fd62e000-64bb-4994-8472-bc4bc6963663/s1200
-
-
-    --====================================================================================
-    local function gradient(buf,vertically,x,y,w,h,white,black,alfa,col,brightness,frame);
-        if h <= 5 then h = 0 end;
-        if w <= 5 then w = 0 end;
-        if white~=true or black~=true then white,black=false,false else white,black=true,true end
-        buf = tonumber(buf)or 948;if buf<0 then buf=0 end;if buf>1023 then buf=1023 end;
-        local b = brightness;
-        alfa = tonumber(alfa)or 1;
-        if alfa < 0 then alfa = 0 elseif alfa > 1 then alfa = 1 end;
-        if type(col) ~= "table" then;
-            col = { {1,0,0},{1,0.25,0},{1,0.5,0},{1,0.75,0},{1,1,0},{0.75,1,0},{0.5,1,0},
-                    {0.25,1,0},{0,1,0},{0,1,0.25},{0,1,0.5},{0,1,0.75},{0,1,1},{0,0.75,1},
-                    {0,0.50,1},{0,0.25,1},{0.25,0,1},{0.5,0,1},{0.75,0,1},{1,0,1},{1,0,0.75},
-                    {1,0,0.5},{1,0,0.25},{1,0,0} };
-        end;
-        -----
-        if white~=true and black~=true then;
-        for i = 1,#col do;
-            for c = 1,3 do;
-                col[i][c] = col[i][c]+b;
-                if col[i][c]>1 then col[i][c]=1 end;
-                if col[i][c]<0 then col[i][c]=0 end;
-            end;
-        end;
-        end;
-        -----
-        local gfx_r,gfx_g,gfx_b,gfx_a = gfx.r,gfx.g,gfx.b,gfx.a;
-        gfx.dest = buf;
-        gfx.setimgdim(buf,-1,-1);
-        local x_s,y_s,w_s,h_s;
-        if vertically == true then;
-            w_s,h_s = 200,10*(#col-1);
-        else;
-            w_s,h_s=10*(#col-1),200;
-        end;
-        gfx.setimgdim(buf,w_s,h_s);
-        gfx.a = 1;
-        -----
-        if vertically ~= true then;
-            ----
-            for i = 1,#col-1 do;
-                gfx.gradrect(0+10*(i-1), 0, 10, 200,
-                             col[i][1],col[i][2],col[i][3], 1,
-                             ((col[i+1][1])-(col[i][1])) / 10,
-                             ((col[i+1][2])-(col[i][2])) / 10,
-                             ((col[i+1][3])-(col[i][3])) / 10,
-                             0);
-            end;
-            ----
-            if white == true then;
-                local r = 1+b;
-                if r < 0 then r = 0 elseif r > 1 then r = 1 end;
-                gfx.gradrect(0,0,(#col-1)*10,100, r,r,r,1, 0,0,0,0, 0,0,0,-1/100);
-            end;
-            ----
-            if black == true then;
-                 local r = 0+b;
-                 if r < 0 then r = 0 elseif r > 1 then r = 1 end;
-                 gfx.gradrect(0,100,(#col-1)*10,100, r,r,r,0, 0,0,0,0, 0,0,0,1/100);
-            end;
-            -----
-        else;
-            -----
-            for i = 1,#col-1 do;
-                gfx.gradrect(0,10*(i-1),200,10,
-                             col[i][1],col[i][2],col[i][3],1, 0,0,0,0,
-                             ((col[i+1][1])-(col[i][1])) / 10,
-                             ((col[i+1][2])-(col[i][2])) / 10,
-                             ((col[i+1][3])-(col[i][3])) / 10, 0);
-            end;
-            ----
-            if white == true then;
-                local r = 0+b;
-                if r < 0 then r = 0 elseif r > 1 then r = 1 end;
-                gfx.gradrect(0,0,100,(#col-1)*10,r,r,r,1, 0,0,0,-1/100, 0,0,0,0);
-            end;
-            ----
-            if black == true then;
-                 local r = 1+b;
-                 if r < 0 then r = 0 elseif r > 1 then r = 1 end;
-                 gfx.gradrect(100,0,100,(#col-1)*10, r,r,r,0, 0,0,0,1/100, 0,0,0,0);
-            end;
-            -----
-        end;
-        gfx.dest = -1;
-        gfx.a = alfa;
-        if vertically == true then;
-            x_s,y_s,w_s,h_s = 1,0,198,10*(#col-1);
-        else;
-            x_s,y_s,w_s,h_s = 0,1,10*(#col-1),198;
-        end;
+        
+        local rgb = (b+1)/2;
+        gfx.gradrect(x,y,w,h, rgb,rgb,rgb,alfa);
+        
         gfx.blit(buf,1,0, x_s,y_s,w_s,h_s, x,y,w,h, 0,0);
         ----
         if type(frame)~="table" then frame = {} end;
