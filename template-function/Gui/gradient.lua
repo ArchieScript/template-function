7aa22749c222d39702d38fc6c8ceafc118414d7a
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2019-11-11 02:14:16 +0800

    Update gradient.lua

--- template-function/Gui/gradient.lua
+++ template-function/Gui/gradient.lua
@@ -20,11 +20,11 @@
     --               Переназначение в виде {{0,0,0},{.5,.5,.5},{1,1,1}}
     -- brightness  / яркость -1..1
     -- frame       / рамка nil или установить в виде {1,1,1,1,1} / {толщина рамки,r,g,b,a}
-    
+    -- UnColGrd   / UnColored Gradient  true включить Неокрашенный Градиент при максимальной, минимальной яркасти (brightness)
     
     
     --====================================================================
-    local function gradient(buf,vertically,x,y,w,h,white,black,alfa,col,brightness,frame);
+    local function gradient(buf,vertically,x,y,w,h,white,black,alfa,col,brightness,frame,UnColGrd);
         if h <= 5 then h = 0 end;
         if w <= 5 then w = 0 end;
         buf = tonumber(buf)or 948;if buf<0 then buf=0 end;if buf>1023 then buf=1023 end;
@@ -33,13 +33,14 @@
         alfa = tonumber(alfa)or 1;
         if alfa < 0 then alfa = 0 elseif alfa > 1 then alfa = 1 end;
         if type(col) ~= "table" or type(col[1])~= "table" then;
-            col = {{1,0,0},{1,.1,0},{1,.2,0},{1,.3,0},{1,.4,0},{1,.5,0},{1,.6,0},{1,.7,0},{1,.8,0},{1,.9,0},
+            col =  {{1,1,1},{1,1,1},{1,.9,.9},{1,.8,.8},{1,.7,.7},{1,.6,.6},{1,.5,.5},{1,.4,.4},{1,.3,.3},{1,.2,.2},{1,.1,.1},
+                   {1,0,0},{1,.1,0},{1,.2,0},{1,.3,0},{1,.4,0},{1,.5,0},{1,.6,0},{1,.7,0},{1,.8,0},{1,.9,0},
                    {1,1,0},{.9,1,0},{.8,1,0},{.7,1,0},{.6,1,0},{.5,1,0},{.4,1,0},{.3,1,0},{.2,1,0},{.1,1,0},
                    {0,1,0},{0,1,.1},{0,1,.2},{0,1,.3},{0,1,.4},{0,1,.5},{0,1,.6},{0,1,.7},{0,1,.8},{0,1,.9},
                    {0,1,1},{0,.9,1},{0,.8,1},{0,.7,1},{0,.6,1},{0,.5,1},{0,.4,1},{0,.3,1},{0,.2,1},{0,.1,1},
                    {0,0,1},{.1,0,1},{.2,0,1},{.3,0,1},{.4,0,1},{.5,0,1},{.6,0,1},{.7,0,1},{.8,0,1},{.9,0,1},
                    {1,0,1},{1,0,.9},{1,0,.8},{1,0,.7},{1,0,.6},{1,0,.5},{1,0,.4},{1,0,.3},{1,0,.2},{1,0,.1},
-                   {1,0,0}};
+                   {1,0,0},{.9,0,0},{.8,0,0},{.7,0,0},{.6,0,0},{.5,0,0},{.4,0,0},{.3,0,0},{.2,0,0},{.1,0,0},{0,0,0},{0,0,0}};
         end;
         if #col == 1 then col[2] = col[1] end;
         -----
@@ -90,6 +91,17 @@
             if rba < 0 then rba = 0 elseif rba > 1 then rba = 1 end;
             gfx.gradrect(0,0,(#col-1)*10,200,0,0,0,rba);
             -----
+            if UnColGrd == true then;
+                gfx.gradrect(0,0,(#col-1)*10,200, rba,rba,rba,rba,
+                             -1/((#col-1)*10),-1/((#col-1)*10),-1/((#col-1)*10),0,
+                             -1/(200),-1/(200),-1/(200),0);
+                local clr = (b-b*2)+1;
+                if clr < 0 then clr = 0 elseif clr > 1 then clr = 1 end;
+                gfx.gradrect(0,0,(#col-1)*10,200,clr,clr,clr,rwa,
+                             1/((#col-1)*10),1/((#col-1)*10),1/((#col-1)*10),0,
+                             1/(200),1/(200),1/(200),0);
+            end;
+            -----
         else;
             -----
             for i = 1,#col-1 do;
@@ -122,6 +134,17 @@
             if black == true then rba = (rba*2-1)end;
             if rba < 0 then rba = 0 elseif rba > 1 then rba = 1 end;
             gfx.gradrect(0,0,200,(#col-1)*10,0,0,0,rba);
+            ----
+            if UnColGrd == true then;
+                local clr = (b-b*2)+1;
+                if clr < 0 then clr = 0 elseif clr > 1 then clr = 1 end;
+                gfx.gradrect(0,0,200,(#col-1)*10, clr,clr,clr,rwa,
+                             1/200,1/200,1/200,0,
+                             1/((#col-1)*10),1/((#col-1)*10),1/((#col-1)*10),0);
+                gfx.gradrect(0,0,200,(#col-1)*10,rba,rba,rba,rba,
+                             -1/200,-1/200,-1/200,0,
+                             -1/((#col-1)*10),-1/((#col-1)*10),-1/((#col-1)*10),0);
+            end;
             ----
         end;
         gfx.dest = -1;
