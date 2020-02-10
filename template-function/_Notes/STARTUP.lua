0db042f97447cc5a13e5378432798b249fe1d22d
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-10 21:09:22 +0800

    Create STARTUP.lua

---
+++ template-function/_Notes/STARTUP.lua
@@ -0,0 +1,85 @@
+    
+    
+    
+    
+    
+    
+    
+    
+    --======================================================================================
+    --////////////  НАСТРОЙКИ  \\\\\\\\\\\\  SETTINGS  ////////////  НАСТРОЙКИ  \\\\\\\\\\\\
+    --======================================================================================
+     
+    
+    local STARTUP = 1;
+    
+    
+    --======================================================================================
+    --////////////// SCRIPT \\\\\\\\\\\\\\  SCRIPT  //////////////  SCRIPT  \\\\\\\\\\\\\\\\
+    --======================================================================================
+    
+    
+    
+    --============== FUNCTION MODULE FUNCTION ========================= FUNCTION MODULE FUNCTION ============== FUNCTION MODULE FUNCTION ==============
+    local Fun,Load,Arc = reaper.GetResourcePath()..'/Scripts/Archie-ReaScripts/Functions'; Load,Arc = pcall(dofile,Fun..'/Arc_Function_lua.lua');--====
+    if not Load then reaper.RecursiveCreateDirectory(Fun,0);reaper.MB('Missing file / Отсутствует файл !\n\n'..Fun..'/Arc_Function_lua.lua',"Error",0);
+    return end; if not Arc.VersionArc_Function_lua("2.6.5",Fun,"")then Arc.no_undo() return end;--=====================================================
+    --============== FUNCTION MODULE FUNCTION ======▲=▲=▲============== FUNCTION MODULE FUNCTION ============== FUNCTION MODULE FUNCTION ==============
+    
+    
+    local scriptPath,scriptName = debug.getinfo(1,'S').source:match("^@(.+)[/\\](.+)");
+    local extname = scriptName;
+     
+    
+    
+    local function body();
+    
+    end;
+    
+    local function FirstRun();
+    
+    end;
+    
+    
+    
+    
+    ---___-----------------------------------------------
+    local FirstRun;
+    if STARTUP == 1 then;
+        --reaper.DeleteExtState(extname,"FirstRun",false);
+        FirstRun = reaper.GetExtState(extname,"FirstRun")=="";
+        if FirstRun then;
+            reaper.SetExtState(extname,"FirstRun",1,false);
+        end;
+    end;
+    -----------------------------------------------------
+    
+    
+    if not FirstRun then;
+        body();
+    elseif FirstRun then;
+        FirstRun();
+    end;
+    
+    
+    ---___-----------------------------------------------
+    local function SetStartupScriptWrite();
+        local id = Arc.GetIDByScriptName(scriptName,scriptPath);
+        if id == -1 or type(id) ~= "string" then Arc.no_undo()return end;
+        local check_Id, check_Fun = Arc.GetStartupScript(id);
+        if STARTUP == 1 then;
+            if not check_Id then;
+                Arc.SetStartupScript(scriptName,id);
+            end;
+        elseif STARTUP ~= 1 then;
+            if check_Id then;
+                Arc.SetStartupScript(scriptName,id,nil,"ONE");
+            end;
+        end;
+    end;
+    reaper.defer(SetStartupScriptWrite);
+    -----------------------------------------------------
+
+
+
+
