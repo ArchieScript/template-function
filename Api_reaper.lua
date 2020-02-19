730f951dc4e855e427b1b266e00e6975a1c8d658
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-02-19 12:46:50 +0800

    Update Api_reaper.lua

--- Api_reaper.lua
+++ Api_reaper.lua
@@ -4,8 +4,8 @@
   -- _,_, scr_x, scr_y = reaper.my_getViewport(0,0, 0, 0,0, 0,0,0, 1 )--monitor resolution  -- разрешение монитора
 
 
-Loopstart,LoopEnd = reaper.GetSet_LoopTimeRange(0,1,0,0,0); -- На Линейке (петля)
-timeSelStart,timeSelEnd = reaper.GetSet_LoopTimeRange(0,0,0,0,0); -- В Аранже
+  -- Loopstart,LoopEnd = reaper.GetSet_LoopTimeRange(0,1,0,0,0); -- На Линейке (петля)
+  -- timeSelStart,timeSelEnd = reaper.GetSet_LoopTimeRange(0,0,0,0,0); -- В Аранже
   
   
   
