2edbf4550bd1095bc1ac616a7b172f99147c2e54
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-05-12 10:11:48 +0800

    Update ini_File.lua

--- template-function/Lua/ini_File.lua
+++ template-function/Lua/ini_File.lua
@@ -7,9 +7,15 @@
 
 
     -------------------------------------------------------------------
-    local function iniFileWrite(section,key,value,iniFile);
+    --boolean 'lua' Необязательный параметр
+    --Если установлен,будет добавлять комментарий 'два тире', полезно для записи в луа файл.
+    --boolean 'clean', true=Очистка пустых строк, Необязательный параметр.
+    -------------------------------------------------------------------
+    local function iniFileWrite(section,key,value,iniFile,lua,clean);
+        if lua==true then lua='--'else lua=''end;
         key = key:gsub('^%s-%;*',''):gsub('\n','');
         value = value:gsub('\n','');
+        section = section:gsub('\n','');
         local file = io.open(iniFile,'r');
         if not file then;
             file = io.open(iniFile,'w');
@@ -22,17 +28,17 @@
         end;
         local t2 = {table.unpack(t)};
         file:close();
-        table.insert(t,'[{3D0dummy085du04EmC390my048E7dummyDF}]');
+        table.insert(t,lua..'[{3D0dummy085du04EmC390my048E7dummyDF}]');
         local section_found;
         for i = 1, #t do;
-            if t[i]:match('^%s-%[%s-'..section..'%s-%]')then;
+            if t[i]:match('^%s-'..lua..'%[%s-'..section..'%s-%]')then;
                 section_found = true;
                 for i2 = i+1,#t do;
-                    if t[i2]:match('^%s-%[')then;
-                        table.insert(t,i2,key..'='..value)break;
+                    if t[i2]:match('^%s-'..lua..'%[')then;
+                        table.insert(t,i2,lua..key..'='..value)break;
                     end;
-                    if t[i2]:match('^%s-'..key..'%s-%=.*')then;
-                        t[i2] = key..'='..value;break;
+                    if t[i2]:match('^%s-'..lua..key..'%s-%=.*')then;
+                        t[i2] = lua..key..'='..value;break;
                     end;
                 end;
                 break;
@@ -40,16 +46,16 @@
         end;
         table.remove(t,#t);
         if not section_found then;
-            table.insert(t,'['..section..']');
-            table.insert(t,key..'='..value);
+            table.insert(t,lua..'['..section..']');
+            table.insert(t,lua..key..'='..value);
         end;
         local write = table.concat(t,'\n');
         if write ~= table.concat(t2,'\n')then;
-            write = write:gsub('\n\n','\n'):gsub('\n\n','\n');
+            if clean==true then write=write:gsub('\n\n','\n'):gsub('\n\n','\n')end;
             file = io.open(iniFile,'w');
-            file:write(write);
+            wrt = file:write(write);
             file:close();
-            return true;
+            return type(wrt)=='userdata';
         end;
         return false;
     end;
