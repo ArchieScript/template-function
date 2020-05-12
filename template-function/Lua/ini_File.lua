97142da752c412f577b922ddbc64dcf5207cebe7
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-05-12 11:06:32 +0800

    Update ini_File.lua

--- template-function/Lua/ini_File.lua
+++ template-function/Lua/ini_File.lua
@@ -13,7 +13,7 @@
     -------------------------------------------------------------------
     local function iniFileWrite(section,key,value,iniFile,lua,clean);
         if lua==true then lua='--'else lua=''end;
-        key = key:gsub('^%s-%;*',''):gsub('\n','');
+        key = key:gsub('^%s-%;*',''):gsub('\n',''):gsub('%=','');
         value = value:gsub('\n','');
         section = section:gsub('\n','');
         local file = io.open(iniFile,'r');
@@ -28,7 +28,7 @@
         end;
         local t2 = {table.unpack(t)};
         file:close();
-        table.insert(t,lua..'[{3D0dummy085du04EmC390my048E7dummyDF}]');
+        table.insert(t,lua..'[{3D0ini0C8File5E8dummyE7CWriteD9F}]');
         local section_found;
         for i = 1, #t do;
             if t[i]:match('^%s-'..lua..'%[%s-'..section..'%s-%]')then;
@@ -58,6 +58,37 @@
             return type(wrt)=='userdata';
         end;
         return false;
+    end;
+    -------------------------------------------------------------------
+    
+    --=================================================================
+    
+    -------------------------------------------------------------------
+    --boolean 'lua' - Смотреть function iniFileWrite, Необязательный параметр
+    -------------------------------------------------------------------
+    local function iniFileRead(section,key,iniFile,lua);
+        if lua==true then lua='--'else lua=''end;
+        key = key:gsub('^%s-%;*',''):gsub('\n',''):gsub('%=','');
+        section = section:gsub('\n','');
+        local file = io.open(iniFile,'r');
+        if not file then return '' end;
+        local t = {};
+        for line in file:lines()do;
+            table.insert(t,line);
+        end;
+        file:close();
+        for i = 1, #t do;
+            if t[i]:match('^%s-'..lua..'%[%s-'..section..'%s-%]')then;
+                for i2 = i+1,#t do;
+                    if t[i2]:match('^%s-'..lua..'%[')then;return''end;
+                    if t[i2]:match('^%s-'..lua..key..'%s-%=.*')then;
+                        return t[i2]:match('^%s-'..lua..key..'%s-%=(.*)');
+                    end;
+                end;
+                break;
+            end;
+        end;
+        return '';
     end;
     -------------------------------------------------------------------
     
