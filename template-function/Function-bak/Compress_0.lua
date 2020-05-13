824b03ed2e58b5bb31aa643b29e7bfc3434f4fd7
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-05-13 20:16:06 +0800

    Update Compress_0.lua

--- template-function/Function-bak/Compress_0.lua
+++ template-function/Function-bak/Compress_0.lua
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 -- Compress  /  Arc_Function_lua
 -- Version: 1.0
 -- Provides: [nomain].
@@ -23,22 +15,69 @@
     -------------
     
     
-    function s(n);return string.rep(" ",n);end;
     
-    
-    ---перенос строки------------
-    text = text:gsub('\n',s(1)) 
-    -----------------------------
+    --- / Header / -----------------
+    local header = string.match(text,"%S.-\n%s-\n"):gsub('\n%s-\n',']]\n')..'\n';
+    -------------------------------- 
     
     
     --- / space / -----------------------------
-    text = text:gsub(s(4),s(1)):gsub(s(3),s(1)):gsub(s(2),s(1)):gsub(s(2),s(1))
+    function s(n);return string.rep(" ",n);end;
     ------------------------------------------- 
+    
+    
+    
+    --- / Удалить Первую Строку / --------------
+    text = text:gsub("local VersionMod.-\n","");
+    --------------------------------------------
+    
+    
+    
+    --- / Комент.Многостр / --------
+    local space = "";
+    for i1 = 1, 5 do;  -- " "
+        local equality = "";
+        for i2 = 1, 100 do; -- "="
+            text = string.gsub(text,"%-%-"..space.."%["..equality.."%[.-%]"..equality.."%]",s(1));
+            equality = equality.."=";
+        end;
+        space = space..s(1);
+    end;
+    --------------------------------
+    
+    
+    --- / Комент.Одностр / -----------
+    text = text:gsub("%-%-.-\n","\n");
+    ----------------------------------
+    
+    
+    --- / Удалить пустые строки  / ---
+    local tx;
+    for var in string.gmatch(text,".-\n")do;
+        var = var:match("%S.*\n")or"\n";
+        tx = (tx or "")..var;
+    end;
+    text = tx;
+    --------------------------------------------
+    
+    
+    --- / Удалить '\n' / -------------
+    text = text:gsub("\n",s(1));
+    ----------------------------------
+    
+    
+    --- / Удалить Пробелы / --------
+    for i = 1,100 do;
+        text = text:gsub(s(2),s(1));
+    end;
+    --------------------------------
+
+    
     
     
     
     --- / Записываем в файл / ------    
     file = io.open(path..'!!! '..scr_file,"w");
-    file:write(text);
+    file:write(header..text);
     file:close();
     --------------------------------
