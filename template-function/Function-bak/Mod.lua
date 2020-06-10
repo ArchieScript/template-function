01689c9cd32917e409bd13a70b4bf81da43f19f9
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-06-11 00:38:36 +0800

    Update Mod.lua

--- template-function/Function-bak/Mod.lua
+++ template-function/Function-bak/Mod.lua
@@ -10,11 +10,12 @@
     --======================================================================================
     
     
+    --=========================================
     local function MODULE(file);
         local E,A=pcall(dofile,file);if not(E)then;reaper.ShowConsoleMsg("\n\nError - "..debug.getinfo(1,'S').source:match('.*[/\\](.+)')..'\nMISSING FILE / ОТСУТСТВУЕТ ФАЙЛ!\n'..file:gsub('\\','/'))return;end;
-        if not A.VersArcFun("2.8.2",file,'')then A.no_undo()return;end;local Arc=E;
-    end;MODULE((reaper.GetResourcePath()..'/Scripts/Archie-ReaScripts/Functions/Arc_Function_lua.lua'):gsub('\\','/'));
-    
+        if not A.VersArcFun("2.8.1",file,'')then A.no_undo()return;end;return A;
+    end;
+    --=========================================
 
 
 
