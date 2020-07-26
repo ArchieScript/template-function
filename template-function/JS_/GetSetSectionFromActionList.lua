c1a3011d106fc9a6a7955ef08a1cda00aad0bc96
ArchieScript <36672393+ArchieScript@users.noreply.github.com>
2020-07-27 02:00:59 +0800

    Create GetSetSectionFromActionList.lua

---
+++ template-function/JS_/GetSetSectionFromActionList.lua
@@ -0,0 +1,24 @@
+   
+   
+   
+   
+   
+   
+   local function GetSetSectionFromActionList(section,set);
+        local hWnd_action = reaper.JS_Window_Find("Actions",true);
+        if not hWnd_action then return end;
+        local combo = reaper.JS_Window_FindChildByID(hWnd_action,1317);
+        if set == 1 or set == true then;
+            reaper.JS_WindowMessage_Send(combo,"CB_SETCURSEL",section or 0,0,0,0);
+        end;
+        local sectionName = reaper.JS_Window_GetTitle(combo,"");
+        local sectionID = reaper.JS_WindowMessage_Send(combo,"CB_GETCURSEL",0,0,0,0);
+        return sectionID,sectionName;
+    end;
+    
+    
+    
+    
+    
+    
+    
